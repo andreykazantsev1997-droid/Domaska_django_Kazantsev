9ef06a1ac824e958c274ee2239b63cf947738014
Andrey Kazantsev <andrey.kazantsev.1997@gmail.com>
2026-08-24 18:56:57 +0300

    add ipython and MEDIA URL, ROOT

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,17 +5,13 @@ from .models import Category, Product
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    # Выводим id и name в списке категорий
     list_display = ('id', 'name')
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    # Выводим id, name, price и category в списке продуктов
     list_display = ('id', 'name', 'price', 'category')
 
-    # Настраиваем фильтрацию продуктов по категории
     list_filter = ('category',)
 
-    # Настраиваем поиск по полям name и description
     search_fields = ('name', 'description')
