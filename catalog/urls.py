8fcd589197fa4c9a1a1177d9d6a86c0e9bb9937c
Andrey Kazantsev <andrey.kazantsev.1997@gmail.com>
2026-08-28 20:30:04 +0300

    update urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,5 +7,5 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path("", home, name='home'),
     path("contacts/", contacts, name='contacts'),
-    path("product/<int:product_id>/", products, name='product')
+    path("product/<int:product_id>/", products, name='product_detail')
 ]
