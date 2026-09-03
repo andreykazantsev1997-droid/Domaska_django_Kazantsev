bfe439c0a74614adc7cd35069e0d6d8cdeccf87b
Andrey Kazantsev <andrey.kazantsev.1997@gmail.com>
2026-09-03 20:40:02 +0300

    modify views.py

---
+++ blog/views.py
@@ -0,0 +1,44 @@
+from django.urls import reverse_lazy
+from .models import Post
+from django.views.generic import CreateView, ListView, DetailView, DeleteView, UpdateView
+
+
+# Create your views here.
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content', 'image']
+    template_name = 'blog/post_form.html'
+    success_url = reverse_lazy('blog:post_list')
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.count_view += 1
+        self.object.save()
+        return self.object
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'content', 'image']
+    template_name = 'blog/post_form.html'
+
+    def get_success_url(self):
+        return reverse_lazy('blog:post_detail', kwargs={'pk': self.object.pk})
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/post_delete.html'
+    success_url = reverse_lazy('blog:post_list')
+
+    def get_queryset(self):
+        return Post.objects.filter(publication_indicator=True)
