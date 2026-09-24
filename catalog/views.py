3f2c6b08ba2dd6a119f20a577e7b21cb7fcdc75a
Andrey Kazantsev <andrey.kazantsev.1997@gmail.com>
2026-09-24 19:46:38 +0300

    add restricted access for unauthorized users

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import DetailView, ListView, TemplateView
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Product
 
 # Create your views here.
@@ -11,7 +11,7 @@ class ProductListView(ListView):
 class ProductTemplateView(TemplateView):
     template_name = 'catalog/contacts.html'
 
-class ProductDetailView(DetailView):
+class ProductDetailView(LoginRequiredMixin, DetailView):
     model = Product
     template_name = 'catalog/products.html'
     context_object_name = 'product'
--- config/settings.py
+++ config/settings.py
@@ -139,6 +139,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 AUTH_USER_MODEL = 'users.CustomUser'
 
 LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = 'users:login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
